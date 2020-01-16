b67638c6b12c14f47fa487abcd08064aaef1d91b
Yeow Jing Jong <yeowjingjong@gmail.com>
2020-01-16 10:59:03 +0800

    added show route

--- server.py
+++ server.py
@@ -1,9 +1,24 @@
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route("/") # Revisit decorators if you unclear of this syntax
+# def index():
+#     return '<h1>Why so easy</h1>'
+
+# if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
+#    app.run()
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/") # Revisit decorators if you unclear of this syntax
+@app.route("/")
 def index():
     return '<h1>Why so easy</h1>'
 
-if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
-   app.run()
+
+@app.route("/another")
+def show():
+    return '<h1>Yo</h1>'
+
+if __name__ == '__main__':
+    app.run()
